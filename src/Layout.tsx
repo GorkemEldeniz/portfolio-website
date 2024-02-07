f794c79a7feb84890d1034aa069320a6ab714207
Görkem Eldeniz <82877261+GorkemEldeniz@users.noreply.github.com>
2024-02-08 00:27:52 +0300

    Hero component & Button component
    Hero component added
    Button component size attribute hot fixed

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,11 @@
+import Hero from "@components/hero";
 import Navbar from "@components/navbar";
 
 const Layout = () => {
 	return (
-		<div className=' min-h-screen max-w-screen-2xl mx-auto bg-background text-foreground'>
+		<div className='min-h-screen max-w-screen-2xl mx-auto bg-background text-foreground'>
 			<Navbar />
+			<Hero />
 		</div>
 	);
 };
---
+++ src/components/hero.tsx
@@ -0,0 +1,27 @@
+import { Button } from "./ui/button";
+
+const Hero = () => {
+	return (
+		<div className='flex flex-col gap-10 px-[3.75rem] py-[1.5rem] font-light text-center'>
+			<h1 className='text-5xl tracking-wide'>Developer Name</h1>
+			<div className='flex justify-center'>
+				<span className='h-[1px] w-[20rem] bg-foreground'></span>
+			</div>
+			<p className='text-lg px-9'>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla interdum
+				urna quis magna lobortis, eget finibus urna vestibulum. Sed nec ex non
+				justo dictum venenatis. Fusce nec purus nec mauris posuere malesuada
+			</p>
+			<div className='flex justify-center gap-6'>
+				<Button className='text-2xl font-light' size='lg' variant='outline'>
+					Projects
+				</Button>
+				<Button className='text-2xl font-light' size='lg'>
+					Contact
+				</Button>
+			</div>
+		</div>
+	);
+};
+
+export default Hero;
