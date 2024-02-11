e6c07bf10f6e74f81e3353ce1858002ed8b36c75
Görkem Eldeniz <82877261+GorkemEldeniz@users.noreply.github.com>
2024-02-11 16:11:12 +0300

    project card design fixed

--- src/components/project-card.tsx
+++ src/components/project-card.tsx
@@ -16,7 +16,7 @@ type Link = {
 	url: `https://${string}`;
 };
 
-interface ProjectCardProps {
+export interface ProjectCardProps {
 	image: Image;
 	title: string;
 	description: string;
@@ -38,39 +38,41 @@ const ProjectCard = ({
 	return (
 		<div className='flex flex-col gap-4 text-left'>
 			<img
-				className='object-cover aspect-square'
+				className='object-contain aspect-square'
 				src={image.src}
 				alt={image.alt}
 			/>
+
 			<h2 className='text-xl md:text-2xl'>{title}</h2>
 			<span className='h-[1px] w-full bg-foreground'></span>
 			<p className='text-sm md:text-base'>{description}</p>
+			<div className='flex flex-col justify-end flex-1 gap-4'>
+				<TechStack<TechStackType>
+					className='flex flex-wrap gap-5'
+					items={stacks}
+					render={(Item) => (
+						<div className='flex flex-col items-center gap-1' key={Item.name}>
+							<Item.icon className='w-5 h-5' key={Item.name} />
+						</div>
+					)}
+				/>
 
-			<TechStack<TechStackType>
-				className='flex flex-wrap gap-5'
-				items={stacks}
-				render={(Item) => (
-					<div className='flex flex-col items-center gap-1' key={Item.name}>
-						<Item.icon className='w-5 h-5' key={Item.name} />
-					</div>
-				)}
-			/>
-
-			<div className='flex gap-2'>
-				<Button
-					onClick={() => handleExternalLink(link.github)}
-					size='icon'
-					variant='link'
-				>
-					<FaGithub className='w-7 h-7' />
-				</Button>
-				<Button
-					onClick={() => handleExternalLink(link.url)}
-					size='icon'
-					variant='link'
-				>
-					<IoIosLink className='w-7 h-7' />
-				</Button>
+				<div className='flex gap-2'>
+					<Button
+						onClick={() => handleExternalLink(link.github)}
+						size='icon'
+						variant='link'
+					>
+						<FaGithub className='w-7 h-7' />
+					</Button>
+					<Button
+						onClick={() => handleExternalLink(link.url)}
+						size='icon'
+						variant='link'
+					>
+						<IoIosLink className='w-7 h-7' />
+					</Button>
+				</div>
 			</div>
 		</div>
 	);
