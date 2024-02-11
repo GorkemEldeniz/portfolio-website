7fdcb7e1c0880c948db600329beb2dc5b272a491
Görkem Eldeniz <82877261+GorkemEldeniz@users.noreply.github.com>
2024-02-11 16:06:13 +0300

    image added

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,17 @@ const Navbar = () => {
 	return (
 		<nav className='flex items-center px-[2rem] md:px-[4rem] py-9'>
 			<div className='flex justify-start flex-1'>
-				<h1 className='text-2xl font-bold md:text-3xl'>Name</h1>
+				<a
+					className='focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring'
+					href='https://www.linkedin.com/in/g%C3%B6rkem-eldeniz-017886218/'
+					target='blank'
+				>
+					<img
+						className='object-cover w-12 h-12 rounded-full'
+						src='../../profile.jpeg'
+						alt='Profile'
+					/>
+				</a>
 			</div>
 
 			<Menu />
