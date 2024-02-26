5e057a820f3583418936fefac3e3e8f13e5f0efe
Görkem Eldeniz <82877261+GorkemEldeniz@users.noreply.github.com>
2024-02-26 09:56:21 +0300

    Meta tags
    meta tags added for enhance the ceo properties

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -2,19 +2,23 @@ import About from "@components/about";
 import Contact from "@components/contact";
 import Foooter from "@components/footer";
 import Hero from "@components/hero";
+import Meta from "@components/meta";
 import Navbar from "@components/nav/navbar";
 import Projects from "@components/project";
 
 const Layout = () => {
 	return (
-		<div className='min-h-screen mx-auto max-w-screen-2xl bg-background text-foreground'>
-			<Navbar />
-			<Hero />
-			<About />
-			<Projects />
-			<Contact />
-			<Foooter />
-		</div>
+		<>
+			<Meta />
+			<div className='min-h-screen mx-auto max-w-screen-2xl bg-background text-foreground'>
+				<Navbar />
+				<Hero />
+				<About />
+				<Projects />
+				<Contact />
+				<Foooter />
+			</div>
+		</>
 	);
 };
 
---
+++ src/components/meta.tsx
@@ -0,0 +1,55 @@
+import { Helmet } from "react-helmet";
+
+const Meta = () => {
+	return (
+		<Helmet>
+			<meta charSet='UTF-8' />
+			<link
+				rel='apple-touch-icon'
+				sizes='180x180'
+				href='/icons/apple-touch-icon.png'
+			/>
+			<link
+				rel='icon'
+				type='image/png'
+				sizes='32x32'
+				href='/icons/favicon-32x32.png'
+			/>
+			<link
+				rel='icon'
+				type='image/png'
+				sizes='16x16'
+				href='/icons/favicon-16x16.png'
+			/>
+			<link rel='manifest' href='/site.webmanifest' />
+			<link
+				rel='mask-icon'
+				href='/icons/safari-pinned-tab.svg'
+				color='#5bbad5'
+			/>
+			<meta name='msapplication-TileColor' content='#da532c' />
+			<meta name='theme-color' content='#ffffff' />
+			<meta name='viewport' content='width=device-width, initial-scale=1.0' />
+			<title>Gorkem Eldeniz</title>
+			<meta name='description' content="Gorkem eldeniz's portfolio" />
+			<meta name='keywords' content='react, meta tags, seo' />
+			<meta name='author' content='Görkem Eldeniz' />
+			<meta property='og:title' content='Gorkem Eldeniz' />
+			<meta property='og:description' content="Gorkem eldeniz's portfolio" />
+			<meta
+				property='og:image'
+				content='https://www.gorkemeldeniz.com/site-image.png'
+			/>
+			<meta property='og:url' content='https://www.gorkemeldeniz.com' />
+			<meta name='twitter:title' content='Gorkem Eldeniz' />
+			<meta name='twitter:description' content="Gorkem eldeniz's portfolio" />
+			<meta
+				name='twitter:image'
+				content='https://www.gorkemeldeniz.com/site-image.png'
+			/>
+			<meta name='twitter:card' content='summary_large_image' />
+		</Helmet>
+	);
+};
+
+export default Meta;
